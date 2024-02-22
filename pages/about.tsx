e4eef647805fb7da3c99c91f8a259c25e446cf74
Ruchir Shah <39463877+ruchirlives@users.noreply.github.com>
2024-02-22 01:41:29 +0000

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is a simple Next.js webpage using the app directory.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
