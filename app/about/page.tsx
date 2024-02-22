5a0e993418559b96fc46abcd0c51699c5c64babb
Ruchir Shah <39463877+ruchirlives@users.noreply.github.com>
2024-02-22 01:51:50 +0000

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Image from "next/image";
 
 const AboutPage: React.FC = () => {
   return (
     <div>
       <h1>About Us</h1>
       <p>This is a simple nested About.tsx webpage using the app directory.</p>
+      <Image/>
     </div>
   );
 }
