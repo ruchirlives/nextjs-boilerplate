debe76e73333de87e84193ceefa7c4c6cd41d9fe
Ruchir Shah <39463877+ruchirlives@users.noreply.github.com>
2024-02-22 01:53:13 +0000

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,13 @@ const AboutPage: React.FC = () => {
     <div>
       <h1>About Us</h1>
       <p>This is a simple nested About.tsx webpage using the app directory.</p>
-      <Image/>
+      <Image
+        src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority/>
     </div>
   );
 }
