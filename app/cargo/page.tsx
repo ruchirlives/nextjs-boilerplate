c4aadc8b6f6bb2bd6cbe4215dab10c224355bbc4
Ruchir Shah <39463877+ruchirlives@users.noreply.github.com>
2024-02-22 21:46:38 +0000

    Update page.tsx

--- app/cargo/page.tsx
+++ app/cargo/page.tsx
@@ -1,15 +1,19 @@
-// app/cargo/page.server.tsx
-import React from 'react';
+// app/cargo/page.tsx
+import React, { useEffect, useState } from 'react';
 import { fetchCargoRecords } from '../../utils/fetchCargoRecords';
-import { useServerInsertedHTML } from 'next/navigation';
 
-export default function CargoPageServer() {
-  const records = useServerInsertedHTML(async () => {
-    const wikiUrl = 'https://digitaltransformation.miraheze.org';
-    const tableName = 'DigitalResource';
-    // Adjust the limit as needed
-    const records = await fetchCargoRecords(wikiUrl, tableName, 80);
-    return records;
+const CargoPage = () => {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const wikiUrl = 'https://digitaltransformation.miraheze.org';
+      const tableName = 'DigitalResource';
+      const data = await fetchCargoRecords(wikiUrl, tableName, 80);
+      setRecords(data);
+    };
+
+    fetchData();
   }, []);
 
   return (
@@ -22,4 +26,6 @@ export default function CargoPageServer() {
       </ul>
     </div>
   );
-}
+};
+
+export default CargoPage;
