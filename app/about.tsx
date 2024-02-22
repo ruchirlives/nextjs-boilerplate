da1e3cc623cf62cf144df5bc8c2fdd68d1d67bf3
Ruchir Shah <39463877+ruchirlives@users.noreply.github.com>
2024-02-22 01:16:23 +0000

    Update and rename About.tsx to about.tsx

---
+++ app/about.tsx
@@ -0,0 +1,9 @@
+// This function can be named anything, but should ideally be descriptive
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is a simple Next.js webpage.</p>
+    </div>
+  );
+}
